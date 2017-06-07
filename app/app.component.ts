28dd383db4ef31368a97a5f9a00758455106be87
Asia Kane and Kat Gifford <kat@>
2017-06-07 09:37:48 -0700

    create Keg class and input hard coded data

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Lotus Kombucha</h1>
+  <h1>Kat & Kane Kombucha Kegs</h1>
+  <h3>Check Out Our Flavors Below!</h3>
+  <div>
+      <p *ngFor="let keg of kegs">{{keg.name}}</p>
+  </div>
   `
 })
 
 export class AppComponent {
+  kegs: Keg[] = [
+    new Keg('Love', 'Brew Dr. Kombucha', 'Jasmine & Lavender', 4, 124),
+    new Keg('Herbal Uplift', 'Brew Dr. Kombucha', 'Green & Peppermint', 4, 124),
+    new Keg('Bliss', 'LionHeart Kombucha', 'Lemon & Lavender', 5, 124),
+  ];
 
+}
+
+export class Keg {
+  tapped: boolean = null;
+  constructor (public name: string, public brand: string, public flavor: string, public price: number, public pints: number) { }
 }
