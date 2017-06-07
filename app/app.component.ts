bb27b6b6bad92a63062d2b1f11a1808383f9c092
Asia Kane and Kat Gifford <kat@>
2017-06-07 11:18:08 -0700

    refactor maste keg list to be available to component

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,15 +6,15 @@ import { Keg }  from './keg.model';
   template: `
   <h1>Kat & Kane Kombucha Kegs</h1>
   <h3>Check Out Our Flavors Below!</h3>
-  <keg-list></keg-list>
+  <keg-list [childKegList]="kegsList"></keg-list>
   `
 })
 
 export class AppComponent {
-  kegs: Keg[] = [
+  kegsList: Keg[] = [
     new Keg('Love', 'Brew Dr. Kombucha', 'Jasmine & Lavender', 4, 124),
     new Keg('Herbal Uplift', 'Brew Dr. Kombucha', 'Green & Peppermint', 4, 124),
     new Keg('Bliss', 'LionHeart Kombucha', 'Lemon & Lavender', 5, 124),
   ];
-  
+
 }
--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Keg } from './keg.model';
 
 @Component({
   selector: 'keg-list',
   template: `
-  <div *ngFor="let keg of kegs" class="container">
+  <div *ngFor="let keg of childKegList" class="container">
     <h4><a (click)="showDetails(keg)">{{keg.name}}</a></h4>
     <button (click)="subtractPint(keg)">Sold Pint</button>
   </div>
@@ -19,11 +19,7 @@ import { Keg } from './keg.model';
 })
 
 export class KegListComponent {
-  kegs: Keg[] = [
-    new Keg('Love', 'Brew Dr. Kombucha', 'Jasmine & Lavender', 4, 124),
-    new Keg('Herbal Uplift', 'Brew Dr. Kombucha', 'Green & Peppermint', 4, 124),
-    new Keg('Bliss', 'LionHeart Kombucha', 'Lemon & Lavender', 5, 124),
-  ];
+  @Input() childKegList: Keg[];
 
   kegToBeShown = null;
 
